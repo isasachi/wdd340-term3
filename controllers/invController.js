a515e9389349986c1cb89b682a24608e92f9fdf1
Isaac Salirrosas <isaacsalirrosasc@gmail.com>
2025-05-28 00:20:20 -0500

    w03 assignment

---
+++ controllers/invController.js
@@ -0,0 +1,35 @@
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
+
+const invCont = {}
+
+/* ***************************
+ *  Build inventory by classification view
+ * ************************** */
+invCont.buildByClassificationId = async function (req, res, next) {
+  const classification_id = req.params.classificationId
+  const data = await invModel.getInventoryByClassificationId(classification_id)
+  const grid = await utilities.buildClassificationGrid(data)
+  let nav = await utilities.getNav()
+  const className = data[0].classification_name
+  res.render("./inventory/classification", {
+    title: className + " Vehicles",
+    nav,
+    grid,
+  })
+}
+
+invCont.buildDetailById = async function (req, res, next) {
+  const inv_id = req.params.invId
+  const data = await invModel.getDetailbyInvId(inv_id)
+  const content = await utilities.buildDetailView(data)
+  let nav = await utilities.getNav()
+  const title = `${data.inv_year} ${data.inv_make} ${data.inv_model}`
+  res.render("./inventory/detail", {
+    title,
+    nav,
+    content
+  })
+}
+
+module.exports = invCont
---
+++ utilities/index.js
@@ -0,0 +1,87 @@
+const invModel = require("../models/inventory-model")
+const Util = {}
+
+/* ************************
+ * Constructs the nav HTML unordered list
+ ************************** */
+Util.getNav = async function (req, res, next) {
+  let data = await invModel.getClassifications()
+  let list = "<ul>"
+  list += '<li><a href="/" title="Home page">Home</a></li>'
+  data.rows.forEach((row) => {
+    list += "<li>"
+    list +=
+      '<a href="/inv/type/' +
+      row.classification_id +
+      '" title="See our inventory of ' +
+      row.classification_name +
+      ' vehicles">' +
+      row.classification_name +
+      "</a>"
+    list += "</li>"
+  })
+  list += "</ul>"
+  return list
+}
+
+/* **************************************
+* Build the classification view HTML
+* ************************************ */
+Util.buildClassificationGrid = async function(data){
+  let grid
+  if(data.length > 0){
+    grid = '<ul id="inv-display">'
+    data.forEach(vehicle => { 
+      grid += '<li>'
+      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+      + 'details"><img src="' + vehicle.inv_thumbnail 
+      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +' on CSE Motors" /></a>'
+      grid += '<div class="namePrice">'
+      grid += '<hr />'
+      grid += '<h2>'
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+      grid += '</h2>'
+      grid += '<span>$' 
+      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      grid += '</div>'
+      grid += '</li>'
+    })
+    grid += '</ul>'
+  } else { 
+    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+  }
+  return grid
+}
+
+Util.buildDetailView = async function (data) {
+  let content
+  if (data) {
+    content = `<div class="inv-detail">
+                <img src="${data.inv_image}" alt="${data.inv_make} ${data.inv_model}">
+                <div class="inv-content">
+                  <h1>${data.inv_year} ${data.inv_make} ${data.inv_model}</h1>
+                  <p>${data.inv_description}</p>
+                  <p>Mileage: ${new Intl.NumberFormat("en-US").format(data.inv_miles)}</p>
+                  <p>Color: ${data.inv_color}</p>
+                  <p class="inv-price">Price: <strong>$${new Intl.NumberFormat("en-US").format(data.inv_price)}</strong></p>
+                  <button>Buy</button>
+                </div>
+              </div>`
+  } else {
+    content = "<p>Sorry, no matching vehicle could be found.</p>"
+  }
+  return content
+}
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+module.exports = Util
